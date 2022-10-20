d59e2d79f64e490bcfa7cf8dd803f4a141ada96b
anagha123 <aadhumkekar_m20@el.vjti.ac.in>
2022-10-20 21:35:04 +0530

    redirect

--- src/client.js
+++ src/client.js
@@ -3,10 +3,7 @@ import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
   projectId: '5dqu2b8j',
-  dataset: 'production',
-  apiVersion: '2022-02-01',
-  useCdn: true,
-  token: process.env.Token,
+  dataset: 'production'
 });
 
 const builder = imageUrlBuilder(client);
